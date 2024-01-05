f1483a9267d062fe575dc90489e5b011c0846cb9
0xDRAGOS <sdragos231@gmail.com>
2024-01-05 19:31:31 +0200

    implement modify button with specific methods; rework login frame; add toStringLine to print line by line movie parameters; add menu with accelerators;

--- Main/src/gui/PageStartPanel.java
+++ Main/src/gui/PageStartPanel.java
@@ -4,6 +4,7 @@ import app.Movie;
 import app.MovieManager;
 import repository.MovieRepository;
 
+import java.awt.event.KeyEvent;
 import java.util.List;
 
 import javax.swing.*;
@@ -32,12 +33,66 @@ public class PageStartPanel extends JPanel {
 
         textArea.setEditable(false);
 
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("Menu");
+        JMenuItem addMenuItem = new JMenuItem("Add");
+        JMenuItem modifyMenuItem = new JMenuItem("Modify");
+        JMenuItem deleteMenuItem = new JMenuItem("Delete");
+        JMenuItem createRaportMenuItem = new JMenuItem("Create Raport");
+
+        menu.add(addMenuItem);
+        menu.add(modifyMenuItem);
+        menu.add(deleteMenuItem);
+        menu.add(createRaportMenuItem);
+        menuBar.add(menu);
         //add(textArea);
+        add(menuBar);
         add(addMovieButton);
         add(modifyMovieButton);
         add(deleteMovieButton);
         add(createRaportButton);
 
+        // menu items
+        addMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()));
+        addMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new AddFrame();
+            }
+        });
+
+        modifyMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_B, Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()));
+        modifyMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new ModifyFrame();
+            }
+        });
+
+        deleteMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()));
+        deleteMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new DeleteFrame();
+            }
+        });
+
+        createRaportMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D, Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()));
+        createRaportMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                List<Movie> movieList = movieRepository.getMovies();
+                movieManager.addAll(movieList);
+
+                if(!movieManager.createRaport("Raports\\movies_raport.txt")){
+                    JOptionPane.showMessageDialog(PageStartPanel.this, "Could not create movie raport.", "Error", JOptionPane.ERROR_MESSAGE);
+                }else{
+                    JOptionPane.showMessageDialog(PageStartPanel.this, "Movie raport created successfully.", "", JOptionPane.INFORMATION_MESSAGE);
+                }
+            }
+        });
+
+        // buttons
         addMovieButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
